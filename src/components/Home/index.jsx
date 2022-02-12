50555d80f2e9f56dcae5aecd022c154ba5781f5c
unknown <juanpabloferroarroyo@gmail.com>
2022-02-12 04:29:27 +0100

    creating some initial structure

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PostsList from "../PostsList";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <PostsList />
+    </div>
+  );
+};
+
+export default Home;
