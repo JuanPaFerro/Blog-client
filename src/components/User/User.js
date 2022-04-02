2ed89606b410e878817114f415193630be9e14fc
unknown <juanpabloferroarroyo@gmail.com>
2022-04-03 00:27:06 +0200

    Styled the User page

---
+++ src/components/User/User.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import BannerImage from "../../assets/post.jpg";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: calc(100vh - 130px);
+`;
+
+export const ProfilePicContainer = styled.div`
+  background-image: url(${BannerImage});
+  background-repeat: no-repeat;
+  background-position: 0;
+  background-size: cover;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 125px;
+`;
+
+export const InfoContainer = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  border-radius: 5px;
+  padding: 2rem;
+`;
+
+export const InfoText = styled.span`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.title};
+  padding: 1rem;
+`;
+export const InfoRoleLabel = styled.div`
+  background-color: ${({ isAdmin }) =>
+    isAdmin ? "rgba(0, 200, 80, 0.9)" : "rgba(50, 120, 255, 0.9)"};
+    font-weight: bold ;
+  padding: 5px 10px;
+  border-radius: 5px;
+  width: fit-content;
+  margin-left: 1rem;
+  color: ${({ theme }) => theme.body};
+`;
+
+export const ProfilePic = styled.img`
+  object-fit: cover;
+  width: 150px;
+  position: relative;
+  bottom: -75px;
+  border-radius: 50%;
+`;
+
+export const LogoutButton = styled.button`
+  background-color: rgba(255, 80, 80, 0.9);
+  font-size: 1.2rem;
+  padding: 5px 10px;
+  border-radius: 5px;
+  border: 1px solid;
+  color: ${({ theme }) => theme.body};
+  margin-top: 20px;
+  font-weight: bold ;
+`;
--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,15 +1,40 @@
 import React, { useContext } from "react";
 import { Context } from "../../Context";
+import {
+  Container,
+  InfoContainer,
+  InfoRoleLabel,
+  InfoText,
+  LogoutButton,
+  ProfilePic,
+  ProfilePicContainer,
+} from "./User";
+import UserImagePlaceholder from "../../assets/user.png";
 
 const User = () => {
   const { removeAuth } = useContext(Context);
+  const username = window.sessionStorage.getItem("username");
+  const image = window.sessionStorage.getItem("image");
+  const email = window.sessionStorage.getItem("email");
+  const role = window.sessionStorage.getItem("role");
+  const publicFiles = "http://localhost:5000/images/";
+
 
   return (
-    <div>
-      <span>User image</span>
-      <span>User Name</span>
-      <button onClick={removeAuth}>Log Out</button>
-    </div>
+    <Container>
+      <ProfilePicContainer>
+        <ProfilePic src={image ? publicFiles + image : UserImagePlaceholder} />
+      </ProfilePicContainer>
+      <InfoContainer>
+        <InfoText>Username: {username}</InfoText>
+        <InfoText>Email: {email}</InfoText>
+        <InfoRoleLabel isAdmin={ role==="1" }>
+          {role === "1" ? "Admin" : "User"}
+        </InfoRoleLabel>
+      </InfoContainer>
+
+      <LogoutButton onClick={removeAuth}>Log Out</LogoutButton>
+    </Container>
   );
 };
 
